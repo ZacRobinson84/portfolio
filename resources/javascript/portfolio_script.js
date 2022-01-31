04bd28aed7f78323f7ac22ead63a983354309b2c
ZacRobinson84 <zacrobinson84@gmail.com>
2022-01-31 00:32:29 -0500

    refactor navbar code -change dropdown menu button to x onclick -change dropdown menu font

--- resources/javascript/portfolio_script.js
+++ resources/javascript/portfolio_script.js
@@ -1,15 +1,23 @@
 function dropMenu() {
   var x = document.getElementById("dropNav");
+  var y = document.getElementById("exitMenu");
   if (x.className === "nav-items") {
     x.className += " responsive";
   } else {
     x.className = "nav-items";
   }
+  if (y.className === "menu-icon") {
+    y.className += " responsive";
+  } else {
+    y.className = "menu-icon";
+  }
 }
 
 function closeDropMenu() {
   var x = document.getElementById("dropNav");
+  var y = document.getElementById("exitMenu");
   x.className = "nav-items";
+  y.className = "menu-icon";
 }
 
 window.addEventListener("resize", closeDropMenu);
